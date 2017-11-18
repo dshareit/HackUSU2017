9758c19bd6ba8c88267689a4de98fcddb5507239
Spencer Thorson <spencermt2012@gmail.com>
2017-11-18 05:17:07 -0700

    added event pages and such

---
+++ lib/event3Page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class event3Page extends StatefulWidget{
+  @override
+  _event3PageState createState() => new _event3PageState();
+}
+
+class _event3PageState extends State<event3Page>{
+  @override
+  Widget build(BuildContext context){
+    return new Row(
+      children: <Widget>[
+        new Text("Work!!"),
+      ],
+    );
+  }
+}
