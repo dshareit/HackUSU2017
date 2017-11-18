7de481f47e96fb037dca3d09af7c501f13a375e5
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-11-17 21:42:00 -0700

    Updated with Google Sign In.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:hackusu2017/firstPage.dart';
 import 'package:hackusu2017/secondPage.dart';
 import 'package:hackusu2017/thirdPage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:async';
 
 void main() {
   runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
 
   @override
   Widget build(BuildContext context) {
@@ -16,15 +20,94 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Secret Santa'),
+      home: signedIn() ? new MyHomePage() : new SignInPage(googleSignIn),
+      routes: <String, WidgetBuilder> { //5
+        '/homePage': (BuildContext context) => new MyHomePage(),
+      },
     );
+  }
+
+  bool signedIn() {
+    GoogleSignInAccount user = googleSignIn.currentUser;
+    return user != null;
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class SignInPage extends StatefulWidget{
 
-  final String title;
+  SignInPage(this.googleSignIn);
+  GoogleSignIn googleSignIn;
+
+  @override
+  _SignInPageState createState() => new _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      body: new Stack(
+        children: <Widget>[
+          new Container(
+            decoration: new BoxDecoration(
+              image : new DecorationImage(
+                image: new ExactAssetImage('assets/background.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+
+              new Container(
+                child: new IconButton(
+                  icon: new Image.asset("assets/google_sign_in_button.png"),
+                  onPressed: _onGoogleSignInPressed,
+                ),
+                width: 1000.0,
+                height: 72.0,
+              )
+
+
+
+            ],
+          ),
+
+        ],
+
+
+      ),
+      
+    );
+  }
+
+  void _switchToHomePage(){
+    Navigator.of(context).pushReplacementNamed('/homePage');
+  }
+
+  Future<Null> _onGoogleSignInPressed() async {
+    GoogleSignInAccount user = widget.googleSignIn.currentUser;
+    if (user == null)
+      user = await widget.googleSignIn.signInSilently();
+
+    if (user == null) {
+      await widget.googleSignIn.signIn();
+    }
+
+    _switchToHomePage();
+  }
+
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  final String title = "Secret Santa";
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
